4d9c13cf376c5c89d881a6306820ae4ab12f268c
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-12-04 18:56:38 -0300

    req 2: user model

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: DataTypes.INTEGER,
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    }, {
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+    });
+
+    return User;
+}
+
+module.exports = UserModel;
